3239802b6ef816239126d295ba16ffd9cebdaafe
Michal.Z <zabrzyjewski@gmail.com>
2020-02-28 13:58:19 +0100

    data binding, basic table layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+interface term {
+  date: string,
+  break?: boolean,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  terms: term[] = [
+    { date:"10:25" },
+    { date: "11:00" },
+    { date: "12:00", break: true },
+    { date: "13:00" },
+    { date: "15:00" }
+  ];
+  studentsCount: number = 8;
+  breakLength: number = 10;
+  startDate: string = "2020-10-10T10:25:00.000";
+  endDate: string = "2020-10-10T13:30:00.000";
 
   constructor() {}
 
